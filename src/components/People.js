933addc67e11a2143b6fe11ea90535c0a9abdd34
Sachu <april.medina2011@gmail.com>
2021-08-27 14:57:28 +0800

    lesson 2

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,30 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
+import Person from './Person';
+
 const People = () => {
+  // const { isLoading, error, data } = useQuery('planets', fetchPlanets);
+
+  const { isLoading, error, data, isFetching } = useQuery("people", () =>
+    fetch(
+      "https://swapi.dev/api/people/"
+    ).then((res) => res.json())
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
     <div>
       <h2>People</h2>
+      {data.results.map(person => <Person key={person.name} person={person} />)}
+      <div>{isFetching ? "Updating..." : ""}</div>
+      {/* <ReactQueryDevtools initialIsOpen /> */}
     </div>
   );
+
 }
- 
+
 export default People;
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,30 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
+import Planet from './Planet';
+
 const Planets = () => {
+  // const { isLoading, error, data } = useQuery('planets', fetchPlanets);
+
+  const { isLoading, error, data, isFetching } = useQuery("planets", () =>
+    fetch(
+      "https://swapi.dev/api/planets/"
+    ).then((res) => res.json())
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return (
     <div>
       <h2>Planets</h2>
+      {data.results.map(planet => <Planet key={planet.name} planet={planet} />)}
+      <div>{isFetching ? "Updating..." : ""}</div>
+      {/* <ReactQueryDevtools initialIsOpen /> */}
     </div>
   );
+
 }
- 
+
 export default Planets;
